d3558ec84254fa106eb3c89d9fe8777e5381149d
FranJMD <fmejia22@ujap.edu.ve>
2024-09-21 18:49:07 -0400

    La Interfaz aparece en pantalla

--- Venv/Front-end.py
+++ Venv/Front-end.py
@@ -3,3 +3,17 @@ import flet as ft
 class ConversorNumeros:
     def __init__(self, page: ft.Page):
         self.page = page
+        self.page.title = "Convertidor de Numeros"
+        self.page.window_width = 780
+        self.page.window_height = 438
+        self.Interfaz
+        
+    def Interfaz(self):
+        Titulo = ft.Text(
+            value="Conversor de Números",
+            size=20
+        )
+        
+def main(page: ft.Page):
+    ConversorNumeros(page)
+ft.app(main)
