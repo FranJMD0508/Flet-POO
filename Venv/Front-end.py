0e2e3d5e7174300c4fa5a2ac7f55cb9c260a3fd1
FranJMD <fmejia22@ujap.edu.ve>
2024-09-21 22:52:58 -0400

    Interfaz personalizada

--- Venv/Front-end.py
+++ Venv/Front-end.py
@@ -3,19 +3,25 @@ import flet as ft
 class ConversorNumeros:
     def __init__(self, page: ft.Page):
         self.page = page
+        self.page.window.center()
         self.page.title = "Convertidor de Numeros"
         self.page.window_width = 764
-        self.page.window_height = 422
+        self.page.window_height = 380
         self.page.window_resizable = False
         self.page.window_maximizable = False
-        self.page.window.center()
+        self.page.bgcolor = "#0E183A"
+        self.page.fonts = {
+            "Inter": "Inter-Regular.ttf"
+        }
         self.Interfaz()
         
     def Interfaz(self):
         ConTxt = ft.Container(
             content = ft.Text(
-                value="Conversor de Números",
-                size = 20),
+                value="CONVERSOR DE NÚMEROS",
+                size = 25,
+                font_family = "Inter",
+                weight = ft.FontWeight.BOLD,),
             alignment = ft.alignment.center
         )
         self.dd1 = ft.Dropdown(
@@ -27,7 +33,12 @@ class ConversorNumeros:
                 ft.dropdown.Option("Cuaternario"),
                 ft.dropdown.Option("Octal")
             ],
-            value = "Binario"
+            value = "Binario",
+            color = "#8B8B8B",
+            fill_color= "#d9d9d9",
+            text_style=ft.TextStyle(
+                weight=ft.FontWeight.BOLD
+            )
         )
         self.dd2 = ft.Dropdown(
             options=[
@@ -38,22 +49,37 @@ class ConversorNumeros:
                 ft.dropdown.Option("Cuaternario"),
                 ft.dropdown.Option("Octal")
             ],
-            value = "Hexadecimal"
+            value = "Hexadecimal",
+            color = "#8b8686",
+            fill_color= "#d9d9d9",
+            text_style=ft.TextStyle(
+                weight=ft.FontWeight.BOLD
+            )
         )
         self.tf1 = ft.TextField(
             hint_text="Escriba aqui...",
+            hint_style=ft.TextStyle(
+                color = "#aeaeae",
+            ),
+            color = "#6c6b6b",
             multiline=True,
-            min_lines=10,
-            max_lines=10,
-            width=300
+            min_lines=8,
+            max_lines=8,
+            width=300,
+            fill_color= "#d9d9d9"
         )
         self.tf2 = ft.TextField(
             hint_text="Aqui aparecerá el numero convertido",
+            hint_style=ft.TextStyle(
+                color = "#aeaeae",
+            ),
+            color = "#6c6b6b",
             multiline=True,
-            min_lines=10,
-            max_lines=10,
+            min_lines=8,
+            max_lines=8,
             width=300,
-            read_only = True
+            read_only = True,
+            fill_color= "#d9d9d9"
         )
         b1 = ft.IconButton(
             icon=ft.icons.ARROW_BACK,
@@ -61,10 +87,14 @@ class ConversorNumeros:
         )
         b2 = ft.ElevatedButton(
             text="Convertir",
+            bgcolor = "#52A644",
+            color = "#FFFFFF"
         )
         b3 = ft.ElevatedButton(
             text="Reiniciar",
-            width = 107
+            width = 107,
+            bgcolor = "#FF4B4B",
+            color = "#FFFFFF"
         )
         self.page.add(ft.Column([
             ConTxt,ft.Column([
